e3332210b5a35a6e9e98f214d90240b7967b8ba0
PabloEnguix09 <pabloenguixllopis@gmail.com>
2025-10-26 18:26:54 +0100

    Projects page done

---
+++ src/layouts/Projects/NutrIA.tsx
@@ -0,0 +1,56 @@
+import { Flex } from '@everyone-web/ui/Common/Flex';
+import { Text, Title } from '@everyone-web/ui/Common/Typography';
+import { Image } from 'antd';
+import movilLanding from '@everyone-web/assets/movilLanding.png';
+
+export const NutrIA = () => {
+  return (
+    <Flex fullScreen vertical gap={16} justify="center" align="center">
+      <Flex fullWidth align="center" justify="space-evenly">
+        <Flex vertical align="center" gap={16} style={{ maxWidth: '30dvw' }}>
+          <Title>NutrIA</Title>
+          <Title level={3}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci totam, obcaecati
+            debitis dicta impedit accusantium magni. Voluptas vero aspernatur cumque iure quam?
+            Numquam aliquam a consectetur placeat quos soluta rem?
+          </Title>
+          <Title level={3}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci totam, obcaecati
+            debitis dicta impedit accusantium magni. Voluptas vero aspernatur cumque iure quam?
+            Numquam aliquam a consectetur placeat quos soluta rem?
+          </Title>
+        </Flex>
+        <Image
+          src={movilLanding}
+          width={530}
+          height={869}
+          preview={false}
+          alt="Móvil landing page"
+          style={{ rotate: '7.5deg' }}
+        />
+        <Flex vertical gap={16} style={{ maxWidth: '30dvw' }}>
+          <Title level={3}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla natus quia ullam, placeat
+            recusandae itaque maxime velit illum. Ut illo blanditiis aperiam nulla expedita omnis
+            delectus quo nihil tempora aliquid.
+          </Title>
+          <Text>Disponible en</Text>
+          <Flex gap={16}>
+            <Image
+              src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+              width={150}
+              preview={false}
+              alt="Download on the App Store"
+            />
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+              width={150}
+              preview={false}
+              alt="Get it on Google Play"
+            />
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,10 +1,24 @@
 import { MainLayout } from '@everyone-web/components/MainLayout/MainLayout';
 import { createFileRoute } from '@tanstack/react-router';
+import { NutrIA } from '@everyone-web/layouts/Projects/NutrIA';
+import { Divider } from 'antd';
+import { Flex } from '@everyone-web/ui/Common/Flex';
+import { EveryLog } from '@everyone-web/layouts/Projects/EveryLog';
 
 export const Route = createFileRoute('/projects')({
   component: Projects,
 });
 
 function Projects() {
-  return <MainLayout></MainLayout>;
+  return (
+    <MainLayout>
+      <Flex vertical align="center">
+        <NutrIA />
+        <Flex style={{ width: '90dvw' }}>
+          <Divider style={{ borderColor: 'var(--color-primary)' }} />
+        </Flex>
+        <EveryLog />
+      </Flex>
+    </MainLayout>
+  );
 }
